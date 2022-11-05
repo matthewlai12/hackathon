f0cace51143be7778a36e728264f1afc20a9147e
Nikolai Philipenko <nikol@LAPTOP-6GDJ0M02.localdomain>
2022-11-05 14:18:20 -0600

    update

--- hackedBeta2022/hackathon_arduino.ino
+++ hackedBeta2022/hackathon_arduino.ino
@@ -1,9 +1,17 @@
+#include <AFMotor.h>
+
+AF_DCMotor left_motor(1);
+
 void setup() {
   // put your setup code here, to run once:
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  left_motor.setSpeed(255);
+  left_motor.run(FORWARD);
+  delay(5000);
+  left_motor.run(RELEASE);
+  delay(5000);
+  
 }
