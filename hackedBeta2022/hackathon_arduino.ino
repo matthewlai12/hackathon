8da3a4a6bb88439e620e655021eae6301726de4a
Nikolai Philipenko <nikol@LAPTOP-6GDJ0M02.localdomain>
2022-11-05 14:42:49 -0600

    update

--- hackedBeta2022/hackathon_arduino.ino
+++ hackedBeta2022/hackathon_arduino.ino
@@ -1,17 +1,21 @@
 #include <AFMotor.h>
+#include <Dabble.h>
 
 AF_DCMotor left_motor(1);
 
 void setup() {
   // put your setup code here, to run once:
+  Dabble.begin(38400);  
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  left_motor.setSpeed(255);
-  left_motor.run(FORWARD);
-  delay(5000);
-  left_motor.run(RELEASE);
-  delay(5000);
+  Dabble.processInput();
+  if GamePad.isUpPressed() {
+    left_motor.setSpeed(255);
+    left_motor.run(FORWARD);
+    left_motor.run(RELEASE);
+  }
   
 }
